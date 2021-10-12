d44c7e7e606a25f627452a26935c15cb7581ea07
Parv Dave <parvdave29@gmail.com>
2021-10-12 23:40:06 +0530

    added twitter analysis

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('',views.landing,name='landing-page'),
-    path('<str:username>',views.analytics_by_account,name='analytics-by-account'),
-    path('<str:username>/<str:mode>',views.likes_per_post,name='likes-per-post')
+    path('user/<str:username>',views.analytics_by_account,name='analytics-by-account'),
+    path('topic/<str:topic>',views.analytics_by_topic,name='analytics-by-topic'),
+    path('user/<str:username>/<str:mode>',views.likes_per_post,name='likes-per-post')
 ]
--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ import io
 import urllib, base64
 from . import quantinsta
 import instaloader
+from . import twitter_functions
 
 insta= instaloader.Instaloader()
 
@@ -11,7 +12,7 @@ insta= instaloader.Instaloader()
 def landing(request):
     if request.method=='POST':
         uid=request.POST.get('uid')
-        return redirect('/'+uid)
+        return redirect(reverse_lazy(f"main:analytics-by-topic '{uid}'"))
     return render(request,'main/index.html',{})
 
 def analytics_by_account(request,username):
@@ -25,6 +26,23 @@ def analytics_by_account(request,username):
     # uri =  urllib.parse.quote(string)
     modes = {'likesperpost':{'name':'Likes per post'},'viewsvslikes':{'name':'Views vs Likes'},'compfollowers':{'name':'Followers comparison'}}
     return render(request,'main/options.html',{'profile':profile,'username':username,'modes':modes})
+
+def analytics_by_topic(request,topic):
+    twitter_functions.twitter_credentials()
+    df,positive,negative,neutral = twitter_functions.twitter_query('covid')
+    df=df.iloc[:,:]
+    fig = twitter_functions.sentplot(df)
+
+    rows=list(df.itertuples(index=False))
+
+    buf = io.BytesIO()
+    fig.savefig(buf,format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri =  urllib.parse.quote(string)
+
+    return render(request,'main/analysis.html',{'graph':uri,'data':rows,'topic':topic,'pos':positive,'neg':negative,'neu':neutral})
+
 
 def likes_per_post(request,username,mode):
     profile = quantinsta.get_profileObject(username)
