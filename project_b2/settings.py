9b181ca63593f83b3937105b3306b0eb111f68ae
rutvikdumre <rutvikdumre@gmail.com>
2022-03-09 17:41:22 +0530

    exception error databse

--- project_b2/settings.py
+++ project_b2/settings.py
@@ -144,3 +144,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'main:landing-page'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+IMPORT_EXPORT_USE_TRANSACTIONS = False
